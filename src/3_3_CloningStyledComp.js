6b49e64e9baa1c17d95c14c1cd827fcbc5219004
SeongYunMin <82751890+SeongYunMin@users.noreply.github.com>
2021-05-12 23:44:19 +0200

    0512_midnight_update

--- src/3_3_CloningStyledComp.js
+++ src/3_3_CloningStyledComp.js
@@ -1,7 +1,7 @@
-// JS로 CSS를 좀 조작해보자!
-let styled = (css) => console.log(css);
+// JS로 CSS와 html을 동시에 조작해보자!
+const styled1 = (css) => console.log(css);
 
-styled`border-radius:10px;
+styled1`border-radius:10px;
 color: blue;
 `;
 // 흠 styled라는 함수 만들었는데 저런식으로 백택으로 된 인자를
@@ -29,8 +29,9 @@ color: blue;
 // 그렇다면 styled 자체가 함수를 return 하는 함수이면 해결할 수 있지 않을까?
 // styled 함수가 return 한 값이 styled(aElement) 라는 함수면 위와 같이 이상하게
 // 쓴 코드를 읽을 수 있지 않을까 따라서 인제는 함수를 return 하는 함수를 만들어보자
+const wrapper5 = document.querySelector(".wrapper5");
 
-styled = (aElement) => {
+const styled = (aElement) => {
   const el = document.createElement(aElement);
   return (args) => {
     const style = args[0];
@@ -43,10 +44,40 @@ styled = (aElement) => {
 // 아래 백택으로 표현한 css 스타일시트이고 이 어래이인 알규먼트의 엘레먼트는
 // string 하나인데 그것을 style에 저장한다(아래 주석에서 계속)
 const title = styled("div")`
-  border-radius: 10px;
-  color: blue;
+  background-color: tomato;
+  color: white;
+  width: 200px;
+  text-align: center;
 `;
 console.log(title);
+title.innerText = "We just cloned";
+wrapper5.append(title);
 // 그리고 그것을 el.style, 즉 div의 style 로 지정한다 그리고 이렇게 style 첨가된
 // el을 다시 return 하게 된다 이러면 콘솔창에 style 첨가된 div태그가 표현된다
 // 이렇게 함수의 함수를 만들 수 있다 이거는 JS로 html과 css를 한꺼번에 조작할 수 있게 해주는듯
+const subtitle = styled("span")`
+  color: blue;
+  font-weight: 600;
+  background-color: black;
+`;
+subtitle.innerText = "Styled Component";
+wrapper5.append(subtitle);
+// 이렇게 함수안의 함수를 쓰고 할 수 있는것이 다 template literal 덕분이다
+
+// 인제 es6에 추가된 4가지 string method 에 대해 알아보자
+// 1. string.includes()
+const isEmail = (email) => email.includes("@");
+console.log(isEmail("andy8809@naver.com"));
+// includes로 @포함됐는지 여부 파악하고 이를 콘솔창에 true false로 출력하게 했다
+
+// 2. string.repeat()
+const CC_NUMBER = "8809";
+const displayName=`${"*".repeat(10)}${CC_NUMBER}`;
+console.log(displayName);
+// 이런식으로 스트링 뒤에 repeat(number) 메소드 쓰면 그만큼 반복 출력해준다
+
+// 3. string.startsWith()
+const namee = "Mr. Min"
+console.log(namee.startsWith("Mr"));
+// 이런식으로 string이 괄호 안의 string으로 시작하는지 여부를 파악한다
+// 같은 맥락으로 마지막 string을 검사하는 endsWith()도 있다
